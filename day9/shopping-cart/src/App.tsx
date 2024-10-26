3af55693ee316b4b32b0768d9fd7465bb36f2c26
Le phuc <lephuc@192.168.2.5>
2024-10-26 17:49:12 +0700

    Day 10 - Typescript, MaterialUI

---
+++ day9/shopping-cart/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material'
+import NavigationBar from './components/NavigationBar'
+import ProductList from './components/ProductList'
+
+
+
+function App() {
+  return (
+    <>
+      <NavigationBar />
+      <ProductList />
+    </>
+  )
+}
+// npm i @emotion/react @emotion/styled @mui/styled-engine-sc styled-components
+export default App
